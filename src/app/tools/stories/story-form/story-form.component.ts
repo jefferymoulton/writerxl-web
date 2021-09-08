455893261a72d7a3703d470196733dbdc33f09b7
jefferymoulton <jeff@jefferymoulton.com>
2021-09-08 13:52:21 -0700

    Created the basic story form.

--- src/app/tools/stories/story-form/story-form.component.ts
+++ src/app/tools/stories/story-form/story-form.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Story } from "../story.model";
 
 @Component({
-  selector: 'app-story-form',
+  selector: 'wxl-story-form',
   templateUrl: './story-form.component.html',
   styleUrls: ['./story-form.component.scss']
 })
 export class StoryFormComponent implements OnInit {
+  story: Story = {
+    ID: 0,
+    title: '',
+    description: '',
+    text: '',
+    created: new Date(),
+  }
 
   constructor() { }
 
--- src/app/tools/stories/story-list/story-list.component.ts
+++ src/app/tools/stories/story-list/story-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { ModalDismissReasons, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'wxl-story-list',
@@ -8,10 +9,31 @@ import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class StoryListComponent implements OnInit {
   faPlusCircle = faPlusCircle;
+  closeResult = '';
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: "lg"}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
