e75d1e196e30f204f233eabe83cb43d23033a404
jefferymoulton <jeff@jefferymoulton.com>
2021-09-08 12:00:23 -0700

    Worked on submission-form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AuthModule } from "@auth0/auth0-angular";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import { environment as env } from '../environments/environment';
-import { ProfileService } from "./shared/services/profile.service";
-import { FooterComponent } from './shared/footer/footer.component';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-import { ToastComponent } from './shared/toast/toast.component';
 import { UserAreaModule } from "./user-area/user-area.module";
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
+import { ToastComponent } from "./shared/toast/toast.component";
+
+import { environment as env } from '../environments/environment';
+
+import { ProfileService } from "./shared/profile.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    NavBarComponent,
     FooterComponent,
+    NavBarComponent,
     ToastComponent,
+    WelcomeComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -40,6 +41,9 @@ import { UserAreaModule } from "./user-area/user-area.module";
     HttpClientModule,
     NgbModule,
     UserAreaModule,
+  ],
+  exports: [
+    NgbModule,
   ],
   providers: [
     ProfileService,
---
+++ src/app/reviewer/submissions/submission-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wxl-reviewer-submission-form',
+  templateUrl: './submission-form.component.html',
+  styleUrls: ['./submission-form.component.scss']
+})
+export class SubmissionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
